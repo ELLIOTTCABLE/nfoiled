fe24fc56f7f546144a5414350cd85802703d926a
elliottcable <git@elliottcable.name>
2009-03-05 22:29:48 -0900

    Pulled all of the module methods into the `class <<self` block

--- lib/nfoiled.rb
+++ lib/nfoiled.rb
@@ -6,42 +6,44 @@ module Nfoiled
   Version = 0
   
   class <<self
-    attr_accessor :initialized
-    alias_method :initialized?, :initialized
+    attr_accessor :initialized; alias_method :initialized?, :initialized
+    attr_accessor :default_terminal
+    
+    ##
+    # This module method is responsible for setting up the entirety of Nfoiled's
+    # overall environment. It will be called before any other Nfoiled
+    # functionality is allowed. In most cases, this will be called for you.
+    # 
+    # This method also schedules `Nfoiled::finalize` to be automatically run
+    # `at_exit`.
+    def initialize!
+      self.initialized = true
+      at_exit { Nfoiled.finalize }
+    end
+    
+    ##
+    # This module method ensures that Nfoiled is initialized. It simply calls
+    # `Nfoiled::initialize!` if Nfoiled hasn't already been initialized.
+    def initialize
+      initialize! unless initialized?
+    end
+    public :initialize
+    
+    ##
+    # This method is responsible for tearing down any environment set up by the
+    # `Ncurses::initialize!` method.
+    def finalize!
+      self.initialized = false
+      ::Ncurses.endwin
+    end
+    
+    ##
+    # This module method ensures that Nfoiled is finalize. It simply calls
+    # `Nfoiled::finalize!` if Nfoiled hasn't already been finalized.
+    def finalize
+      # TODO: Ensure finalization on fatal errors or interrupts
+      finalize! if initialized?
+    end
   end
   
-  ##
-  # This module method is responsible for setting up the entirety of Nfoiled's
-  # overall environment. It will be called before any other Nfoiled
-  # functionality is allowed. In most cases, this will be called for you.
-  # 
-  # This method also schedules `Nfoiled::finalize` to be automatically run
-  # `at_exit`.
-  def self.initialize!
-    self.initialized = true
-    at_exit { Nfoiled.finalize }
-  end
-  
-  ##
-  # This module method ensures that Nfoiled is initialized. It simply calls
-  # `Nfoiled::initialize!` if Nfoiled hasn't already been initialized.
-  def self.initialize
-    initialize! unless initialized?
-  end
-  
-  ##
-  # This method is responsible for tearing down any environment set up by the
-  # `Ncurses::initialize!` method.
-  def self.finalize!
-    self.initialized = false
-    ::Ncurses.endwin
-  end
-  
-  ##
-  # This module method ensures that Nfoiled is finalize. It simply calls
-  # `Nfoiled::finalize!` if Nfoiled hasn't already been finalized.
-  def self.finalize
-    # TODO: Ensure finalization on fatal errors or interrupts
-    finalize! if initialized?
-  end
 end
