e862e0c1463077a6d5cf0ce20fa8a9fc4600016e
elliottcable <git@elliottcable.name>
2009-03-05 21:01:11 -0900

    Getting a start on initialization routines.

--- lib/nfoiled.rb
+++ lib/nfoiled.rb
@@ -5,5 +5,24 @@ require 'ncurses'
 module Nfoiled
   VERSION = 0
   
+  ##
+  # This module method is responsible for setting up the entirety of Nfoiled's
+  # overall environment. It will be called before any other Nfoiled
+  # functionality is allowed. In most cases, this will be called for you.
+  def self.initialize!
+    @@initialized = true
+  end
   
+  ##
+  # This module method ensures that Nfoiled is initialized. It simply calls
+  # `Nfoiled::initialize!` if Nfoiled hasn't already been initialized.
+  def self.initialize
+    initialize! unless @@initialized
+  end
+  
+  ##
+  # This method is
+  def self.finalize!
+    @@initialized = false
+  end
 end
