3d013012bd08a4c649645befb5356a977dfffda2
elliottcable <git@elliottcable.name>
2009-03-05 22:49:06 -0900

    Documented `Window`

--- lib/nfoiled/window.rb
+++ lib/nfoiled/window.rb
@@ -1,6 +1,9 @@
 module Nfoiled
   ##
-  # `Nfoiled::Window`
+  # An `Nfoiled::Window` is a "box" in the terminal to which output can be
+  # printed and from which input can be received. A basic Nfoiled application
+  # will utilize only one of these, a single `Window` covering the entirety
+  # of the `Terminal`'s available area.
   class Window
     
   end
