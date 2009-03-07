d141a5b7d9e552cb2580ba04fffe87116c240177
elliottcable <git@elliottcable.name>
2009-03-07 04:29:48 -0900

    Added a basic multiple terminals example

---
+++ examples/multiple_terminals.rb
@@ -0,0 +1,22 @@
+($:.unshift File.expand_path(File.join( File.dirname(__FILE__), '..', 'lib' ))).uniq!
+require 'nfoiled'
+
+##
+# This example deals with utilizing multiple `Terminal` instances in Nfoiled.
+
+# Creating our first `Terminal` will take care of initializing the Nfoiled
+# system for us.
+term1 = Nfoiled::Terminal.new
+term2 = Nfoiled::Terminal.new
+
+# Rembmer that creating a new `Terminal` also activates it, so `term2` is
+# active right now!
+::Ncurses.doupdate
+
+sleep 5
+
+# Now let's switch to `term1`.
+term1.activate!
+::Ncurses.doupdate
+
+sleep 5
