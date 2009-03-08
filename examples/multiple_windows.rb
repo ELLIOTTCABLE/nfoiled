988a3a1ae66fdaf74be0e91b37d13e3819be3503
elliottcable <git@elliottcable.name>
2009-03-08 13:23:59 -0800

    Added a multiple_windows example

---
+++ examples/multiple_windows.rb
@@ -0,0 +1,32 @@
+($:.unshift File.expand_path(File.join( File.dirname(__FILE__), '..', 'lib' ))).uniq!
+require 'nfoiled'
+
+##
+# This example shows you how to utilize a single Nfoiled window
+
+# Creating our first `Window` should take care of initializing the Nfoiled
+# system for us. However, the `Ncurses.LINES` and `Ncurses.COLS` methods won't
+# be defined until the system is initialized, so we have to prime it.
+Nfoiled::initialize
+left  = Nfoiled::Window.new :top => 0,
+                            :left => 0,
+                            :height => ::Ncurses.LINES,
+                            :width => ::Ncurses.COLS / 2
+right = Nfoiled::Window.new :top => 0,
+                            :left => ::Ncurses.COLS / 2,
+                            :height => ::Ncurses.LINES,
+                            :width => ::Ncurses.COLS / 2
+
+# As per usual, the screen doesn't update until we actually tell it to.
+::Ncurses.doupdate
+
+# Let's print some stuff, just to see
+left.print  "left-brain"
+right.print "right-brain"
+
+# ... aaaaand update again!
+left.wrapee.wnoutrefresh
+right.wrapee.wnoutrefresh
+::Ncurses.doupdate
+
+sleep 10
