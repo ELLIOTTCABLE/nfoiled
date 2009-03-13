7e744220a825ce259a2e4456fa1a2019d026fc72
elliottcable <git@elliottcable.name>
2009-03-13 13:39:21 -0400

    Re-named some keys

--- lib/nfoiled/key.rb
+++ lib/nfoiled/key.rb
@@ -3,18 +3,19 @@ module Nfoiled
   # This is the class of a single character of input received by Nfoiled.
   # Handles modifiers for you.
   class Key
-    Names = {       0 => :nul,  1 => :soh,  2 => :stx,  3 => :etx,
-        4 => :eot,  5 => :enq,  6 => :ack,  7 => :bel,  8 => :bs ,
+    Names = {       0 => :null, 1 => :soh,  2 => :stx,  3 => :etx,
+        4 => :eot,  5 => :enq,  6 => :ack,  7 => :bell, 8 => :backspace,
        10 => :nl , 11 => :vt , 12 => :np , 13 => :cr , 14 => :so ,
        15 => :si , 16 => :dle, 17 => :dc1, 18 => :dc2, 19 => :dc3,
        20 => :dc4, 21 => :nak, 22 => :syn, 23 => :etb, 24 => :can,
-       25 => :em , 26 => :sub, 27 => :esc, 28 => :fs , 29 => :gs ,
-       30 => :rs , 31 => :us ,127 => :del}
+       25 => :em , 26 => :sub, 27 => :escape,28 => :fs,29 => :gs ,
+       30 => :rs , 31 => :us ,127 => :delete}
     
     ##
     # Responsible for processing input from `Nfoiled::read!`. Returns
     def self.process charint
       new case charint
+        when nil;        then :null
         when 9, 32..126; then charint.chr
         else                  Names[charint]
       end
