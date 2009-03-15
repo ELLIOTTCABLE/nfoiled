de1cd8b4b1c2db132661de2c6ba064c9e32a9df8
elliottcable <git@elliottcable.name>
2009-03-14 21:45:22 -0400

    Wrapped `Nfoiled::read!`'s contents in a block so that it can actually use the new asynchronous `Window#getk` interface.

--- lib/nfoiled.rb
+++ lib/nfoiled.rb
@@ -74,10 +74,11 @@ module Nfoiled
     # (if such a block has been defined). See `getch(3X)`.
     def read!
       while true
-        key = Terminal.current.acceptor.getk
-        # TODO: This should be handled more naturally by a Key handler or something
-        exit if [Key.new(:etx), Key.new(:eot)].include? key # ^C, ^D
-        Terminal.current.acceptor.on_key[key] if key
+        Terminal.current.acceptor.getk do |key|
+          # TODO: This should be handled more naturally by a Key handler or something
+          exit if [Key.new(:etx), Key.new(:eot)].include? key # ^C, ^D
+          Terminal.current.acceptor.on_key[key]
+        end
       end
     end
   end
