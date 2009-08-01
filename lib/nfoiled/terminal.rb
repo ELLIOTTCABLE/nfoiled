f053eb98455973f53eaeeae4cf69125fe6ca128e
elliottcable <git@elliottcable.name>
2009-07-31 16:39:33 -0800

    No need to explicitly define the acceptor accessors; the acceptor is defined in #initialize

--- lib/nfoiled/terminal.rb
+++ lib/nfoiled/terminal.rb
@@ -38,10 +38,6 @@ module Nfoiled
     # The `Window` responsible for accepting input to this Terminal. Defaults
     # to the last created `Window`, unless one has been defined.
     attr_accessor :acceptor
-    def acceptor; @acceptor || windows.last; end
-    def acceptor=(window)
-      @acceptor = window
-    end
     
     ##
     # Responsible for creating a new `Terminal`. See `newterm(3X)`.
